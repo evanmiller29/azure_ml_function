ddc30eaa44320f80999dd98277431c899295c57b
evanmiller29 <evanmiller29@gmail.com>
2019-11-16 12:14:21 +0000

    Updating repo with Azure Function bare bones and model build

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # azure_ml_function
 Code required for deploying titanic model to Azure Functions
+
+The data used for this is from the Titanic kaggle competition.
+
+It can be found in either of these two locations:
+    - https://www.kaggle.com/c/titanic/data
+    -
---
+++ titanic_model/model_deployment/model_build.py
@@ -0,0 +1,44 @@
+# Taken from
+# https://scikit-learn.org/stable/auto_examples/compose/plot_column_transformer_mixed_types.html#sphx-glr-auto-examples-compose-plot-column-transformer-mixed-types-py
+
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import joblib
+
+titanic_url = ('https://raw.githubusercontent.com/amueller/'
+               'scipy-2017-sklearn/091d371/notebooks/datasets/titanic3.csv')
+data = pd.read_csv(titanic_url)
+
+numeric_features = ['age']
+numeric_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='median')),
+    ('scaler', StandardScaler())])
+
+categorical_features = ['embarked', 'sex', 'pclass']
+categorical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))])
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numeric_transformer, numeric_features),
+        ('cat', categorical_transformer, categorical_features)])
+
+# Append classifier to preprocessing pipeline.
+# Now we have a full prediction pipeline.
+clf = Pipeline(steps=[('preprocessor', preprocessor),
+                      ('classifier', LogisticRegression(solver='lbfgs', random_state=1234))])
+
+X = data[['age', 'embarked', 'sex', 'pclass']]
+y = data['survived']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+
+clf.fit(X_train, y_train)
+print("model score: %.3f" % clf.score(X_test, y_test))
+joblib.dump(clf, 'titanic_model_pipe.pkl')
