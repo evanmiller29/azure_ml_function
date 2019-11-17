ae3bf15875744f4811964543a6017e5846a07fd8
evanmiller29 <evanmiller29@gmail.com>
2019-11-17 13:22:20 +0000

    Adding in Azure URL

--- titanic_model/api_test.py
+++ titanic_model/api_test.py
@@ -2,6 +2,8 @@ import requests
 import json
 
 local_url = "http://localhost:7071/api/model_deployment"
+azure_url = "YOUR_AZURE_URL_PLUS_CODE"
+
 data = [
     {
     'pclass': '1',
@@ -30,5 +32,9 @@ data = [
 ]
 
 r = requests.post(local_url, json=json.dumps(data))
+print(r)
+print(r.text)
+
+r = requests.post(azure_url, json=json.dumps(data))
 print(r)
 print(r.text)
